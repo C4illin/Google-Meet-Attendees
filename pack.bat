68036cd6497eb952e5fdea952ae671e3be9bed8b
C4illin <emrik.ostling@gmail.com>
2020-04-12 18:23:46 +0200

    updated extension script

--- pack.bat
+++ pack.bat
@@ -1,3 +1,7 @@
+COPY copy.simple.user.js gmeet-ext\js\
 crx3 -p gmeet.pem gmeet-ext
+DEL gmeet-ext\js\copy.simple.user.js
+EXIT
 
+REM with chrome ↓
 REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --pack-extension=%CD%\gmeet-ext
