131b7cb845d9b2e5a228f7e541a0c84e0e26f9c9
C4illin <emrik.ostling@gmail.com>
2020-04-13 00:23:13 +0200

    replaced buttons with "a" elements

--- copy.simple.user.js
+++ copy.simple.user.js
@@ -16,9 +16,63 @@ let peopleList = null
 let peopleCounter = null
 let compare = null
 let nameSelector = null
+let includeYourself = localStorage.getItem("gma-include-yourself") === "true"
 
 const icon =
   "<path fill=\"currentColor\" d=\"M11.41 3.76c-.8.04-1.6.31-2.27.86-1.5 1.22-1.89 3.52-.59 5.06 1.06 1.24 3.02 1.55 4.3.4.98-.88 1.21-2.5.2-3.52a2.05 2.05 0 00-1.34-.6c-.5-.02-1.05.17-1.42.61-.23.29-.35.64-.33 1.01.01.38.23.82.65 1.01.32.14.52.1.78-.01a.7.7 0 00.39-.37.74.74 0 00-.07-.65l-.84.54a.41.41 0 01-.01-.3c.05-.11.12-.13.13-.14l.04.02c-.07-.03-.07-.04-.07-.14s.05-.27.1-.33a.67.67 0 01.6-.25c.24.02.51.13.69.3.56.57.41 1.55-.18 2.08-.82.74-2.14.53-2.85-.3-.92-1.09-.63-2.76.45-3.64 1.34-1.09 3.37-.73 4.42.6 1.25 1.6.82 3.98-.77 5.2l.61.79a4.73 4.73 0 00.94-6.6 4.31 4.31 0 00-3.56-1.63zm.44 9.55c-1.42 0-3.45.34-5.19 1.04-.87.35-1.67.79-2.28 1.35a2.9 2.9 0 00-1.03 2.11v3.5h17v-3.5a2.9 2.9 0 00-1.04-2.11c-.6-.56-1.4-1-2.27-1.35a15.08 15.08 0 00-5.2-1.04zm0 1c1.25 0 3.22.33 4.81.97.8.32 1.5.72 1.97 1.15.48.44.72.89.72 1.38v2.5h-15v-2.5c0-.5.24-.94.71-1.38a6.57 6.57 0 011.97-1.15c1.6-.64 3.57-.97 4.82-.97zm0 1c-1.43 0-2.92.34-4.11.77-.6.21-1.11.45-1.51.7-.4.25-.74.45-.86.9l-.02.08v1.55h13v-1.57l-.02-.06c-.13-.47-.46-.66-.87-.9-.4-.25-.91-.49-1.5-.7a12.56 12.56 0 00-4.11-.77zm0 1c1.27 0 2.68.31 3.77.7.54.2 1 .42 1.32.62.3.19.42.38.4.3v.38h-11v-.37c0 .07.1-.12.41-.3.32-.2.79-.42 1.33-.62 1.09-.4 2.5-.7 3.77-.7z\"></path>"
+
+const s = document.createElement("style")
+s.innerText = `
+#attendees-list a {
+  display: inline-block;
+  margin: 0 2px;
+  padding: 0 5px;
+  border-radius: 2rem;
+  color: var(--gm-body-text-color);
+  cursor: pointer;
+  line-height: 1.5rem;
+  background-color: gainsboro;
+}
+
+#attendees-list p {
+  margin: 0;
+}
+
+#attendees-list #update {
+  display: inline-block;
+  width: 34px;
+  height: 34px;
+  border-radius: 2rem 0 0 2rem;
+  background-image: url('https://img.icons8.com/material/24/000000/update-left-rotation.png');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-color: gainsboro;
+}
+
+#attendees-list #update:hover, #attendees-list #show_list:hover, #attendees-list a:hover {
+  background-color: dimgrey;
+}
+
+#attendees-list #show_list {
+  display: inline-flex;
+  border-radius: 0 2rem 2rem 0;
+  height: 34px;
+  position: absolute;
+  background-color: gainsboro;
+  padding: 0 9px 0 9px;
+  margin-left: 2px;
+  line-height: 34px;
+}
+
+.__gmgv-button > div {
+  border-radius: 0 0 8px 8px !important;
+}
+
+.__gmgv-button:hover {
+  background-color: var(--gm-neutral-highlight-color);
+}
+`
+document.body.append(s)
 
 setInterval(() => {
   let buttons = document.querySelector("[data-fps-request-screencast-cap]").parentElement.parentElement.parentElement
@@ -31,6 +85,16 @@ setInterval(() => {
     toggleButton.classList = buttons.children[1].classList
     toggleButton.classList.add("__gmgv-button")
     toggleButton.style.display = "flex"
+    toggleButton.onclick = () => {
+      let elem = document.getElementById("attendees-list")
+      if (elem.pinned) {
+        elem.style.display = null
+        elem.pinned = false
+      } else {
+        elem.style.display = "block"
+        elem.pinned = true
+      }
+    }
     buttons.prepend(toggleButton)
 
     toggleButtonSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg")
@@ -53,7 +117,6 @@ setInterval(() => {
     
     const showListI = document.createElement("a")
     showListI.id = "show_list"
-    showListI.type = "button"
     showListI.innerText = "Visa/Göm Lista"
     showListI.onclick = () => {
       showElement(peopleList)
@@ -67,9 +130,7 @@ setInterval(() => {
     let attendees = localStorage.getItem("gmca-attendees-list")
     if (attendees) {
       peopleList.value = attendees.replace(/,/g, String.fromCharCode(13, 10))
-      // peopleCounter.innerText = "hmm personer"
       peopleCounter.innerText = (attendees.length - attendees.replace(/,/g, "").length + 1) + " personer"
-      // peopleCounter.innerText(attendees.split(",").length + " personer")
     } else {
       peopleList.value = "Uppdatera listan"
       peopleCounter.innerText = "0 personer"
@@ -79,17 +140,26 @@ setInterval(() => {
     additionalOptions.appendChild(peopleList)
     additionalOptions.appendChild(peopleCounter)
 
-    const copyList = document.createElement("input")
-    copyList.type = "button"
-    copyList.value = "Kopiera lista"
+    const includeYourselfLabel = document.createElement("label")
+    const includeYourselfCheck = document.createElement("input")
+    includeYourselfCheck.type = "checkbox"
+    includeYourselfCheck.checked = includeYourself
+    includeYourselfCheck.onchange = e => {
+      includeYourself = e.target.checked
+      localStorage.setItem("gma-include-yourself", includeYourself)
+    }
+    includeYourselfLabel.innerText = "Inkluder dig själv"
+    additionalOptions.appendChild(includeYourselfCheck)
+
+    const copyList = document.createElement("a")
+    copyList.innerText = "Kopiera lista"
     copyList.onclick = () => {
       navigator.clipboard.writeText(localStorage.getItem("gmca-attendees-list").replace(/,/g, "\n"))
     }
     additionalOptions.appendChild(copyList)
 
-    const randomPerson = document.createElement("input")
-    randomPerson.type = "button"
-    randomPerson.value = "Slumpa person"
+    const randomPerson = document.createElement("a")
+    randomPerson.innerText = "Slumpa person"
     randomPerson.onclick = () => {
       let attendees = localStorage.getItem("gmca-attendees-list").split(",")
       setTimeout(() => { // to make it async
@@ -98,9 +168,8 @@ setInterval(() => {
     }
     additionalOptions.appendChild(randomPerson)
 
-    const showCompareList = document.createElement("input")
-    showCompareList.type = "button"
-    showCompareList.value = "Visa jämförings Lista"
+    const showCompareList = document.createElement("a")
+    showCompareList.innerText = "Visa jämförings Lista"
     showCompareList.onclick = () => {
       showElement(compare)
     }
@@ -116,11 +185,15 @@ setInterval(() => {
     compareList.style.display = "block"
     compare.appendChild(compareList)
 
-    const compareButton = document.createElement("input")
-    compareButton.type = "button"
-    compareButton.value = "Jämför"
+    const compareButton = document.createElement("a")
+    compareButton.innerText = "Jämför"
     compareButton.onclick = compareLists
     compare.appendChild(compareButton)
+
+    const cleanCompare = document.createElement("a")
+    cleanCompare.innerText = "Städa jämföringslista"
+    cleanCompare.onclick = cleanCompareLists
+    compare.appendChild(cleanCompare)
 
     const hereL = document.createElement("label")
     hereL.innerText = "Resultat:"
@@ -133,17 +206,15 @@ setInterval(() => {
     compareResultList.style.display = "block"
     compare.appendChild(compareResultList)
 
-    const copyCompareList = document.createElement("input")
-    copyCompareList.type = "button"
-    copyCompareList.value = "Kopiera lista"
+    const copyCompareList = document.createElement("a")
+    copyCompareList.innerText = "Kopiera lista"
     copyCompareList.onclick = () => {
       navigator.clipboard.writeText(compare.children[compare.childElementCount-3].value)
     }
     compare.appendChild(copyCompareList)
 
-    const copyCompareListForChat = document.createElement("input")
-    copyCompareListForChat.type = "button"
-    copyCompareListForChat.value = "Kopiera (max 500 tecken)"
+    const copyCompareListForChat = document.createElement("a")
+    copyCompareListForChat.innerText = "Kopiera för chatten"
     copyCompareListForChat.onclick = () => {
       let toCopy = compare.children[compare.childElementCount-3].value
       while (toCopy.length > 500) {
@@ -155,37 +226,12 @@ setInterval(() => {
     compare.appendChild(copyCompareListForChat)
 
     additionalOptions.appendChild(compare)
-
-    const s = document.createElement("style")
-    s.innerText = `
-    #update {
-      display: inline-block;
-      width: 34px;
-      height: 34px;
-      border-radius: 2rem 0 0 2rem;
-      background-image: url('https://img.icons8.com/material/24/000000/update-left-rotation.png');
-      background-repeat: no-repeat;
-      background-position: center center;
-      background-color: gainsboro;
-    }
-    
-    #update:hover , #show_list:hover {
-      background-color: dimgrey;
-    }
-    
-    #show_list {
-      display: inline-flex;
-      border-radius: 0 2rem 2rem 0;
-      height: 30px;
-      position: absolute;
-      background-color: gainsboro;
-      padding: 0 9px 0 9px;
-      margin-left: 2px;
-    }
-    `
-    document.body.append(s)
   }
 }, 250)
+
+const cleanCompareLists = () => {
+  compare.firstChild.value = compare.firstChild.value.replace(/\.|[0-9]/g, "").replace(/\n{2,}|\n{1,} {1,}/g, "\n").replace(/ {2,}/g, " ")
+}
 
 const showElement = (elem) => {
   if (elem.style.display === "none") {
