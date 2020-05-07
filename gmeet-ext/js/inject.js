a1468648dd207761b82f0e25bbbbab56a15dbd2a
C4illin <emrik.ostling@gmail.com>
2020-05-07 14:09:20 +0200

    simplified it a bit

--- gmeet-ext/js/inject.js
+++ gmeet-ext/js/inject.js
@@ -1,25 +1,8 @@
-function injectScript(file_path, tag="html", type="script", text="") {
-    
-  var node = document.getElementsByTagName(tag)[0]
-  var tag_type = type == "link" ? "link" : "script"
-  var script = document.createElement(tag_type)
-  if(type == "script") {
-    script.setAttribute("type", "text/javascript")
-  }
-  else if (type == "module") {
-    script.setAttribute("type", "module")
-  }
-  else {
-    script.setAttribute("rel", "stylesheet")
-    script.setAttribute("media", "screen")
-        
-  }
-  if (text == "") {
-    script.setAttribute(tag_type == "script" ? "src": "href", file_path)
-  }
-  else {
-    script.innerHTML = text
-  }
+function injectScript(file_path) {
+  const node = document.getElementsByTagName("html")[0]
+  const script = document.createElement("script")
+  script.setAttribute("type", "text/javascript")
+  script.setAttribute("src", file_path)
   node.appendChild(script)
 }
 
