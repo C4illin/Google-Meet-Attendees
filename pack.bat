901ba3107205556b453b7a93865060ad84784363
C4illin <emrik.ostling@gmail.com>
2020-05-12 16:39:28 +0200

    Updated extension

--- pack.bat
+++ pack.bat
@@ -1,9 +1,10 @@
 DEL gmeet-ext.zip
 COPY copy.simple.user.js gmeet-ext\js\
 powershell Compress-Archive gmeet-ext\. gmeet-ext.zip
+crx3 -p gmeet.pem gmeet-ext
 DEL gmeet-ext\js\copy.simple.user.js
 EXIT
 
-REM crx3 -p gmeet.pem gmeet-ext
+REM 
 REM with chrome ↓
 REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --pack-extension=%CD%\gmeet-ext
