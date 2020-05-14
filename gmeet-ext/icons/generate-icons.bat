0616709b4fc86bb9ab6560b3234af76bb16add17
C4illin <emrik.ostling@gmail.com>
2020-05-14 15:54:39 +0200

    Added comment about svg style

--- gmeet-ext/icons/generate-icons.bat
+++ gmeet-ext/icons/generate-icons.bat
@@ -3,3 +3,6 @@ inkscape -z -w 96 -h 96 icon.svg -e icon96.png
 inkscape -z -w 48 -h 48 icon.svg -e icon48.png
 inkscape -z -w 32 -h 32 icon.svg -e icon32.png
 inkscape -z -w 16 -h 16 icon.svg -e icon16.png
+
+
+REM The icon can't have any inline styles
