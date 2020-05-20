1cdb9754dd6b6764ffcb6b18541d5cbe4eb3bd7e
itggot-daniel-persson <59963395+itggot-daniel-persson@users.noreply.github.com>
2020-05-21 00:39:12 +0200

    dark mode tweak

--- copy.simple.user.js
+++ copy.simple.user.js
@@ -185,7 +185,8 @@ const translations = {
   },
   "sort compare list by status": {
     en: "Sort compare list by status",
-    sv: "Sortera jämföringslistan efter status"
+    sv: "Sortera jämföringslistan efter status",
+    de: "Sortierliste nach Status sortieren"
   }
 }
 
@@ -402,18 +403,27 @@ h1, h2, h3, #attendees-list p {
   background-image: url('data:image/svg+xml;utf8,<svg fill="%23FFFFFF" xmlns="http://www.w3.org/2000/svg" viewBox="-4 -4 32 32"><path d="M13.85 22.25h-3.7c-.74 0-1.36-.54-1.45-1.27l-.27-1.89c-.27-.14-.53-.29-.79-.46l-1.8.72c-.7.26-1.47-.03-1.81-.65L2.2 15.53c-.35-.66-.2-1.44.36-1.88l1.53-1.19c-.01-.15-.02-.3-.02-.46 0-.15.01-.31.02-.46l-1.52-1.19c-.59-.45-.74-1.26-.37-1.88l1.85-3.19c.34-.62 1.11-.9 1.79-.63l1.81.73c.26-.17.52-.32.78-.46l.27-1.91c.09-.7.71-1.25 1.44-1.25h3.7c.74 0 1.36.54 1.45 1.27l.27 1.89c.27.14.53.29.79.46l1.8-.72c.71-.26 1.48.03 1.82.65l1.84 3.18c.36.66.2 1.44-.36 1.88l-1.52 1.19c.01.15.02.3.02.46s-.01.31-.02.46l1.52 1.19c.56.45.72 1.23.37 1.86l-1.86 3.22c-.34.62-1.11.9-1.8.63l-1.8-.72c-.26.17-.52.32-.78.46l-.27 1.91c-.1.68-.72 1.22-1.46 1.22zm-3.23-2h2.76l.37-2.55.53-.22c.44-.18.88-.44 1.34-.78l.45-.34 2.38.96 1.38-2.4-2.03-1.58.07-.56c.03-.26.06-.51.06-.78s-.03-.53-.06-.78l-.07-.56 2.03-1.58-1.39-2.4-2.39.96-.45-.35c-.42-.32-.87-.58-1.33-.77l-.52-.22-.37-2.55h-2.76l-.37 2.55-.53.21c-.44.19-.88.44-1.34.79l-.45.33-2.38-.95-1.39 2.39 2.03 1.58-.07.56a7 7 0 0 0-.06.79c0 .26.02.53.06.78l.07.56-2.03 1.58 1.38 2.4 2.39-.96.45.35c.43.33.86.58 1.33.77l.53.22.38 2.55z"></path><circle cx="12" cy="12" r="3.5"></circle></svg>')
 }
 
+#attendees-list.dark_mode #compare-div {
+  border-color: #dcdcdc63;
+  border-color: #0000006b;
+}
+
 #attendees-list.dark_mode a, #attendees-list.dark_mode #update, #attendees-list.dark_mode #show-list {
   background-color: #38393f;
   color: white;
-  box-shadow: -3px -3px 3px 0 rgba(0, 0, 0, .09), 3px 3px 3px 0 rgba(255, 255, 255, 0.15);
+  box-shadow: none;
 }
 
 #attendees-list.dark_mode textarea, #attendees-list.dark_mode input, #attendees-list.dark_mode select, #attendees-list.dark_mode select option {
   color: white;
   background-color: #181e23;
-  border: none;
+  border-color: #0000003d;
 }
 
+::placeholder { 
+  color: lightgrey;
+  opacity: 1; 
+}
 `
 document.body.append(s)
 
