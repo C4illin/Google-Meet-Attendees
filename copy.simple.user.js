2333d3de28506e804752ea91c2c41a5866af62a5
C4illin <emrik.ostling@gmail.com>
2020-06-08 18:49:29 +0200

    working swap in table

--- copy.simple.user.js
+++ copy.simple.user.js
@@ -1156,11 +1156,48 @@ const printOutGroupsPart2 = (groups, meets) => {
     for (let k = 0; k < groups[i].length; k++) {
       let tableRowGroupNames = addElement("tr",table,null,null)
       for (let l = i; l < Math.min(i + 3,groups.length); l++) {
-        addElement("td",tableRowGroupNames,null,(groups[l] ?? [])[k] ?? "" )
+        let tableData = addElement("td",tableRowGroupNames,null,(groups[l] ?? [])[k] ?? "" )
+        createTableDataExtras(tableData)
       }
     }
     addElement("tr",table,null,null)
   }
+}
+
+const createTableDataExtras = (tableData) => {
+  tableData.draggable = true
+  tableData.ondragover = (ev => ev.preventDefault())
+  tableData.ondrop = (ev => {
+    ev.preventDefault()
+    // console.log(ev.target)
+    // console.log(ev.dataTransfer.getData("text"))
+    // let elementTarget = ev.target.parentElement.nextSibling
+    let toSwap = ev.target.innerText
+    let newName = ev.dataTransfer.getData("text")
+
+    for (const elem of document.querySelectorAll("td")) {
+      if (elem.innerText == newName) {
+        console.log(elem)
+        elem.innerText = toSwap
+        break
+      } else {
+        console.log(elem.innerText)
+      }
+    }
+
+    ev.target.innerText = newName
+    
+    // let elem = document.createElement("td")
+    // elem.innerText = ev.dataTransfer.getData("text")
+    // ev.target.parentElement.nextSibling.prepend(elem)
+    // createTableDataExtras(tableData)
+  })
+
+  tableData.ondragstart = (ev => {
+    console.log(ev.target)
+    ev.dataTransfer.setData("text", ev.target.innerText)
+  })
+
 }
 
 const copyGroups = (groups) => {
