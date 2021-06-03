5027f3d2ba3d7e2e3f87cd44c9483ab68fc722ff
C4illin <emrik.ostling@gmail.com>
2021-06-04 00:03:10 +0200

    removed microsoft

--- extensions/microsoft/js/inject.js
+++
@@ -1,9 +0,0 @@
-function injectScript(file_path) {
-  const node = document.getElementsByTagName("html")[0]
-  const script = document.createElement("script")
-  script.setAttribute("type", "text/javascript")
-  script.setAttribute("src", file_path)
-  node.appendChild(script)
-}
-
-injectScript(chrome.runtime.getURL("js/copy.simple.user.js"))
